5d0c52f4a47c1277c111b0b744a0b30de425b49a
dima10241024 <56293464+dima10241024@users.noreply.github.com>
2020-10-08 11:33:10 +0300

    Update dimaGraphi.c

--- dimaGraphi.c
+++ dimaGraphi.c
@@ -42,9 +42,12 @@ int main(void){
 			
 		for (i = 0; i < N; i++)
 			for (j = 0; j < N; j++) {
-				if (A[i][j] == 1){
+				if (A[i][j] != 0){
+                                        while(A[i][j]>=1){
 					fprintf(s, "%d -> %d; \n", i + 1, j + 1);
 					//printf("%d -> %d; \n", i+1, j+1);
+                                        A[i][j]--;
+                                        }
 				}
 			}
 		fprintf(s, "}");
